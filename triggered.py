4fc226cf8a58b5b67ed5ac6f91b348b8af66a2f8
Daniel Popescu <dpopes@yelp.com>
2019-03-08 16:33:15 -0800

    Add fixes for transparency

--- triggered.py
+++ triggered.py
@@ -13,23 +13,40 @@ def main():
 
     frames = []
 
+    # this image should already have a transparent background
+    # if it doesn't, then learn to use Preview
     orig = Image.open(args.image)
     orig_w, orig_h = orig.size
 
+    # trans MUST be zero, or first position in palette, for slack to respect gif transparency
+    trans = 0
+    disp = 3
+    palette = []
+
+    color_not_in_original_image = (255, 0, 0, 0)
+
     for i in range(10):
-        triggered = Image.new(
-            'RGBA', (orig_w + args.magnitude * 2, orig_h + args.magnitude * 2), (255,255,255,0))
+        triggered = Image.new('RGBA', (orig_w + args.magnitude, orig_h + args.magnitude), color_not_in_original_image)
 
         trig_w, trig_h = triggered.size
 
-        pos_x = ((trig_w - orig_w) // 2) + randrange(-args.magnitude, args.magnitude)
-        pos_y = ((trig_h - orig_h) // 2) + randrange(-args.magnitude, args.magnitude)
+        pos_x = randrange(0, args.magnitude)
+        pos_y = randrange(0, args.magnitude)
 
-        triggered.paste(orig, (pos_x, pos_y), orig)
+        triggered.alpha_composite(orig, (pos_x, pos_y))
+        trig_conv = triggered.convert(mode='P')
+        trig_conv_palette = trig_conv.getpalette()
+
+        if i == 0:
+            # debug statement to make sure that chosen color for trans is in first position of palette
+            print trig_conv_palette
+            palette = trig_conv_palette
+
         frames.append(triggered)
 
-    frames[0].save('{0}.triggered.gif'.format(args.image), format='GIF', disposal=3, append_images=frames[1:],
-                   transparency=0, save_all=True, duration=60, loop=0)
+    frames[0].save('{0}.triggered.gif'.format(args.image), format='GIF', disposal=disp, append_images=frames[1:],
+                   transparency=trans, save_all=True, duration=50, loop=0)
 
 if __name__ == '__main__':
     main()
+
